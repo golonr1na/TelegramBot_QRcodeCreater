a870cc9cb0c620fbeb26c7316c702c24641c84f7
Elizaveta Kashilova <lizun.kashka@gmail.com>
2024-03-02 18:36:13 +0500

    Bot_ver3

--- QRcodeCreate.py
+++ QRcodeCreate.py
@@ -3,7 +3,9 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import CircleModuleDrawer
 
 img = ''
-def Create_circle_QR(txt):
+
+
+def create_circle_qr(txt):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,11 +15,13 @@ def Create_circle_QR(txt):
     qr.add_data(txt)
     qr.make(fit=True)
     # Преобразование в png
-    img = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer(), fill_color="black", back_color="white")
+    img = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer(), fill_color="black",
+                        back_color="white")
     qr.clear()
     img.save('photo.png')
 
-def Crate_regular_QR(txt):
+
+def create_regular_qr(txt):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
