fe9fc5fac04b375dc6516740bb6d7cb2dc423968
Elizaveta Kashilova <lizun.kashka@gmail.com>
2024-03-03 17:17:35 +0500

    Добавление коментариев

--- QRcodeCreate.py
+++ QRcodeCreate.py
@@ -2,18 +2,22 @@ import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import CircleModuleDrawer
 
-typeiscircle = False
+typeiscircle = False # проверка типа qr-кода
 def create_qr(txt):
+    # параметры изображения QR-кода
     qr = qrcode.QRCode(
         error_correction=qrcode.constants.ERROR_CORRECT_M,
         box_size=30,
         border=2,
     )
+    # созание кода
     qr.add_data(txt)
     qr.make(fit=True)
+    # Проверка на тип QR-кода
     if typeiscircle:
         img = qr.make_image(image_factory=StyledPilImage, module_drawer=CircleModuleDrawer())
     else:
         img = qr.make_image(fill_color="black", back_color="white")
+
     qr.clear()
     img.save('qr-code.png')
